b48222991577b98842f81a568fa49fc373685184
kitutune <p.kitutune@gmail.com>
2023-06-10 10:10:38 +0900

    Route設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
-
+import Main from "./components/Main";
 function App() {
   return (
-    <>
-      <div>
-        <SignIn />
-        <SignUp />
-        <Main />
-        <NotFound />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
